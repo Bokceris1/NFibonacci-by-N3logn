e40aaed5ffecec511eff43d85a2bc5fc4da70b9b
Ivan Kostin <Bokceris@yandex.ru>
2025-09-13 14:15:52 +0300

    Add files via upload

--- src/nfib.h
+++ src/nfib.h
@@ -6,119 +6,12 @@
 #include <iterator>
 #include <numeric>
 #include <iostream>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace boost::multiprecision;
 
 template <typename T, size_t SIDE>
 class SquareMatrix {
-private:
-  template <typename N>
-  class BasicColumnIterator {
-  public:
-    using value_type = T;
-    using reference = N&;
-    using pointer = N*;
-    using difference_type = std::ptrdiff_t;
-    using iterator_category = std::random_access_iterator_tag;
-    using ConstColumnIterator = BasicColumnIterator<const N>;
-
-    reference operator[](size_t off) {
-      return *(*this + off);
-    }
-
-    reference operator[](size_t off) const {
-      return *(*this + off);
-    }
-
-    reference operator*() const {
-      return _data[_offset];
-    }
-
-    pointer operator->() const {
-      return _data + _offset;
-    }
-
-    operator ConstColumnIterator() const {
-      return ConstColumnIterator(_data, _cols, _offset);
-    }
-
-    BasicColumnIterator& operator++() {
-      _data += _cols;
-      return *this;
-    }
-
-    BasicColumnIterator operator++(int) {
-      BasicColumnIterator result = *this;
-      ++(*this);
-      return result;
-    }
-
-    BasicColumnIterator& operator--() {
-      _data -= _cols;
-      return *this;
-    }
-
-    BasicColumnIterator operator--(int) {
-      BasicColumnIterator result = *this;
-      --(*this);
-      return result;
-    }
-
-    BasicColumnIterator& operator=(const BasicColumnIterator& other) = default;
-
-    friend BasicColumnIterator operator+(const BasicColumnIterator& left, difference_type right) {
-      return {left._data + (right * static_cast<difference_type>(left._cols)), left._cols, left._offset};
-    }
-
-    friend BasicColumnIterator operator-(const BasicColumnIterator& left, difference_type right) {
-      return left + (-right);
-    }
-
-    friend BasicColumnIterator operator+(difference_type left, const BasicColumnIterator& right) {
-      return right + left;
-    }
-
-    friend BasicColumnIterator operator-(difference_type left, const BasicColumnIterator& right) {
-      return right - left;
-    }
-
-    friend difference_type operator+(const BasicColumnIterator& left, const BasicColumnIterator& right) {
-      return (left._data + right._data) / left._cols;
-    }
-
-    friend difference_type operator-(const BasicColumnIterator& left, const BasicColumnIterator& right) {
-      return (left._data - right._data) / static_cast<difference_type>(left._cols);
-    }
-
-    BasicColumnIterator& operator+=(difference_type other) {
-      *this = *this + other;
-      return *this;
-    }
-
-    BasicColumnIterator& operator-=(difference_type other) {
-      *this = *this - other;
-      return *this;
-    }
-
-    friend bool operator==(const BasicColumnIterator& lhs, const BasicColumnIterator& rhs) = default;
-
-    friend auto operator<=>(const BasicColumnIterator& lhs, const BasicColumnIterator& rhs) = default;
-
-    BasicColumnIterator() = default;
-
-    BasicColumnIterator(const BasicColumnIterator&) = default;
-
-  private:
-    BasicColumnIterator(pointer data, std::size_t cols, std::size_t col)
-        : _data(data)
-        , _cols(cols)
-        , _offset(col) {}
-
-    pointer _data;
-    std::size_t _cols;
-    std::size_t _offset;
-
-    friend class SquareMatrix;
-  };
-
 public:
   using ValueType = T;
 
@@ -128,204 +21,154 @@ public:
   using Pointer = T*;
   using ConstPointer = const T*;
 
-  using Iterator = T*;
-  using ConstIterator = const T*;
-
-  using RowIterator = T*;
-  using ConstRowIterator = const T*;
-
-  using ColIterator = BasicColumnIterator<T>;
-  using ConstColIterator = BasicColumnIterator<const T>;
-
-public:
-  SquareMatrix()
-      : _data(new T[SIDE * SIDE]{}) {
-    for (Iterator it = begin(); it < end(); ++it) {
-      *it = 0;
+  SquareMatrix() {
+    for (size_t i = 0; i < SIDE * SIDE; ++i) {
+      _data[i] = 0;
     }
   }
 
-  SquareMatrix(const SquareMatrix& other)
-      : SquareMatrix() {
-    std::copy(other.begin(), other.end(), begin());
-  }
-
-  SquareMatrix& operator=(const SquareMatrix& other) {
-    if (this != &other) { 
-      SquareMatrix tmp = other;
-      swap(*this, tmp);
-    }
-    return *this;
-  }
-
-  friend void swap(SquareMatrix& lhs, SquareMatrix& rhs) 
-  {
-    std::swap(lhs._data, rhs._data);
-  }
-
-  ~SquareMatrix() {
-    delete[] _data;
-  }
-
-  // Iterators
-
-  Iterator begin() {
-    return _data;
-  }
-
-  ConstIterator begin() const {
-    return _data;
-  }
-
-  Iterator end() {
-    return begin() + size();
-  }
-
-  ConstIterator end() const {
-    return begin() + size();
-  }
-
-  RowIterator row_begin(size_t row) {
-    return begin() + row * side();
-  }
-
-  ConstRowIterator row_begin(size_t row) const {
-    return begin() + row * side();
-  }
-
-  RowIterator row_end(size_t row) {
-    return row_begin(row) + side();
-  }
-
-  ConstRowIterator row_end(size_t row) const {
-    return row_begin(row) + side();
-  }
-
-  ColIterator col_begin(std::size_t col) {
-    return {begin(), side(), col};
-  }
-
-  ColIterator col_end(std::size_t col) {
-    return {end(), side(), col};
-  }
-
-  ConstColIterator col_begin(std::size_t col) const {
-    return {begin(), side(), col};
-  }
-
-  ConstColIterator col_end(std::size_t col) const {
-    return {end(), side(), col};
-  }
-
-  // Size
-
-  size_t side() const {
-    return SIDE;
-  }
-
-  size_t size() const {
-    return side() * side();
-  }
-
-  bool empty() const {
-    return size() == 0;
-  }
-
-  // Elements access
-
-  Reference operator()(size_t row, size_t col) {
+  Reference operator[](size_t row, size_t col) {
     return _data[row * SIDE + col];
   }
 
-  ConstReference operator()(size_t row, size_t col) const {
+  ConstReference operator[](size_t row, size_t col) const {
     return _data[row * SIDE + col];
-  }
-
-  Pointer data() {
-    return _data;
-  }
-
-  ConstPointer data() const {
-    return _data;
   }
 
   // mul
 
-  SquareMatrix& operator*=(const SquareMatrix& other) {
-    SquareMatrix m = *this * other;
-    swap(*this, m);
-    return *this;
-  }
-
   friend SquareMatrix operator*(const SquareMatrix& left, const SquareMatrix& right) {
-    SquareMatrix m = SquareMatrix();
-    for (std::size_t row = 0; row < SIDE; ++row) {
-      for (std::size_t col = 0; col < SIDE; ++col) {
-        m._data[(row * SIDE) + col] =
-            std::inner_product(right.col_begin(col), right.col_end(col), left.row_begin(row), T());
+    SquareMatrix m;
+    for (std::size_t i = 0; i < SIDE; ++i) {
+      for (std::size_t k = 0; k < SIDE; ++k) {
+        T a_ik = left[i, k];
+        for (std::size_t j = 0; j < SIDE; ++j) {
+          m[i, j] += a_ik * right[k, j];
+        }
       }
-    }
-    return m;
-  }
-
-  friend std::array<T, SIDE> operator*(const std::array<T, SIDE>& left, const SquareMatrix& right) {
-    std::array<T, SIDE> m;
-    for (std::size_t col = 0; col < SIDE; ++col) {
-      m[col] = std::inner_product(right.col_begin(col), right.col_end(col), left.begin(), T());
     }
     return m;
   }
 
   SquareMatrix genIdentity() {
     SquareMatrix id;
-    for (size_t i = 0; i < side(); ++i) {
-      id(i, i) = 1;
+    for (size_t i = 0; i < SIDE; ++i) {
+      id[i, i] = 1;
     }
     return id;
   }
 
   SquareMatrix pow(size_t power) {
     SquareMatrix result = genIdentity();
+
     SquareMatrix base = *this;
+
     while (power > 0) {
       if (power & 1) {
-        result *= base;
+        result = result * base;
       }
-      base *= base;
-      power = power / 2;
+      base = base * base;
+      power = power >>= 1;
     }
+
     return result;
   }
 
 private:
-  T* _data;
+  std::array<T, SIDE * SIDE> _data;
 };
 
-// Generalization of the Binet formula
-template <size_t N>
-SquareMatrix<size_t, N> genBase() {
-  SquareMatrix<size_t, N> base;
+template <typename IntType, size_t N>
+SquareMatrix<IntType, N> genBase() {
+  SquareMatrix<IntType, N> base;
   for (size_t i = 0; i < N; ++i) {
-    base(i, 0) = 1;
+    base[0, i] = 1;
   }
   for (size_t i = 1; i < N; ++i) {
-    base(i - 1, i) = 1;
+    base[i, i - 1] = 1;
   }
   return base;
 }
 
-template <size_t N>
-size_t fib(size_t n) {
+template <typename IntType, size_t N>
+IntType fib_iterative(size_t n) {
+  if (n == 0) {
+    return 0;
+  }
+
+  if (n <= N) {
+    return 1;
+  }
+
+  std::array<IntType, N> base;
+  base.fill(1);
+
+  for (size_t i = 0; i < n - N - 1; ++i) {
+    IntType sum = 0;
+    for (size_t j = 0; j < N; ++j) {
+      sum += base[j];
+      if (j != N - 1) {
+        base[j] = base[j + 1];
+      }
+    }
+    base[N - 1] = sum;
+  }
+  return base[N - 1];
+}
+
+template <typename IntType>
+IntType fib_impl(size_t n) {
+    IntType a = 0, b = 1;
+    IntType bit = 1;
+    
+    while (bit <= n / 2) {
+        bit <<= 1;
+    }
+
+    while (bit > 0) {
+        IntType c = a * (2 * b - a); 
+        IntType d = a * a + b * b; 
+        if (n & bit) {
+            a = d;
+            b = c + d;
+        } else {
+            a = c;
+            b = d;
+        }
+        bit >>= 1;
+    }
+    return a;
+}
+
+template <typename IntType, size_t N>
+IntType fib(size_t n) {
 	if (n == 0) {
-		return 0;
+	    return 0;
 	}
+
+    if (N == 2) {
+      return fib_impl<IntType>(n - 1);
+    }
+
     if (n <= N) {
       return 1;
     }
-    SquareMatrix matrix = genBase<N>();
-    std::array<size_t, N> first{};
-    first.fill(1);
-    SquareMatrix powered = matrix.pow(n - N);
-    first = first * powered;
-    return first[0];
+
+    if (pow(N, 3) * log2(n) > N * n) {
+      return fib_iterative<IntType, N>(n);
+    }
+
+    SquareMatrix matrix = genBase<IntType, N>();
+
+    SquareMatrix powered = matrix.pow(n - N - 1);
+
+    IntType sum = 0;
+    for (size_t i = 0; i < N; ++i) {
+      sum += powered[0, i];
+    }
+
+    return sum;
 }
+
+
+
--- test/tests.cpp
+++ test/tests.cpp
@@ -1,108 +1,136 @@
 #include "nfib.h"
 
 #include <gtest/gtest.h>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using namespace boost::multiprecision;
 
 namespace {
 
-template <size_t N>
-size_t fib_correct(size_t n) {
+template <typename IntType, size_t N>
+IntType fib_correct(size_t n) {
+  if (n == 0) {
+    return 0;
+  }
+
   if (n <= N) {
     return 1;
   }
-  std::vector<size_t> base(N, 1);
-  for (size_t i = 0; i < n - N; ++i) {
-    size_t sum = 0;
-    for (size_t j = 1; j <= N; ++j) {
-      sum += base[base.size() - j];
+
+  std::array<IntType, N> base;
+  base.fill(1);
+
+  for (size_t i = 0; i < n - N - 1; ++i) {
+    IntType sum = 0;
+    for (size_t j = 0; j < N; ++j) {
+      sum += base[j];
+      if (j != N - 1) {
+        base[j] = base[j + 1];
+      }
     }
-    base.push_back(sum);
+    base[N - 1] = sum;
   }
-  return base[base.size() - 1];
+  return base[N - 1];
 }
 
 } // namespace
 
-TEST(correctness, 11_element) {
-  size_t n = 11;
-  size_t answer = 89;
-  EXPECT_EQ(fib<2>(n), answer);
-}
-
-TEST(correctness, 93_element) {
-  size_t n = 93;
-  size_t answer = 12200160415121876738ULL;
-  EXPECT_EQ(fib<2>(n), answer);
-}
-
-TEST(correctness, 5_trielement) {
-  size_t n = 5;
-  size_t answer = 5;
-  EXPECT_EQ(fib<3>(n), answer);
-}
-
-TEST(correctness, 6_trielement) {
-  size_t n = 6;
-  size_t answer = 9;
-  EXPECT_EQ(fib<3>(n), answer);
+TEST(correctness, 2test) {
+  for (size_t j = 2; j < 93; ++j) {
+    EXPECT_EQ((fib<size_t, 2>(j)), (fib_correct<size_t, 2>(j)));
+  }
 }
 
 TEST(correctness, 7_trielement) {
   size_t n = 7;
-  EXPECT_EQ(fib<3>(n), fib_correct<3>(n));
-}
-
-TEST(correctness, 8_trielement) {
-  size_t n = 8;
-  size_t answer = 31;
-  EXPECT_EQ(fib<3>(n), answer);
-}
-
-TEST(correctness, 9_trielement) {
-  size_t n = 9;
-  size_t answer = 57;
-  EXPECT_EQ(fib<3>(n), answer);
-}
-
-TEST(correctness, 10_trielement) {
-  size_t n = 10;
-  size_t answer = 105;
-  EXPECT_EQ(fib<3>(n), answer);
-}
-
-TEST(correctness, 11_trielement) {
-  size_t n = 11;
-  size_t answer = 193;
-  EXPECT_EQ(fib<3>(n), answer);
-}
-
-TEST(correctness, 20_trielement) {
-  size_t n = 20;
-  size_t answer = 46499;
-  EXPECT_EQ(fib<3>(n), answer);
+  EXPECT_EQ((fib<size_t, 3>(n)), (fib_correct<size_t, 3>(n)));
 }
 
 TEST(correctness, 35_trielement) {
   size_t n = 35;
-  EXPECT_EQ(fib<3>(n), fib_correct<3>(n));
+  EXPECT_EQ((fib<size_t, 3>(n)), (fib_correct<size_t, 3>(n)));
 }
 
 TEST(correctness, 90_trielement) {
   size_t n = 90;
-  EXPECT_EQ(fib<3>(n), fib_correct<3>(n));
+  EXPECT_EQ((fib<size_t, 3>(n)), (fib_correct<size_t, 3>(n)));
 }
 
 TEST(correctness, 10_4element) {
   size_t n = 10;
-  EXPECT_EQ(fib<4>(n), fib_correct<4>(n));
+  EXPECT_EQ((fib<size_t, 4>(n)), (fib_correct<size_t, 4>(n)));
 }
 
 TEST(correctness, 5_10element) {
   size_t n = 5;
-  EXPECT_EQ(fib<10>(n), fib_correct<10>(n));
+  EXPECT_EQ((fib<size_t, 10>(n)), (fib_correct<size_t, 10>(n)));
 }
 
-TEST(correctness, bigtest) {
-    for (size_t j = 7; j < 30; ++j) {
-      EXPECT_EQ(fib<7>(j), fib_correct<7>(j));
+TEST(correctness, 7test) {
+    for (size_t j = 6; j < 30; ++j)
+    {
+    EXPECT_EQ((fib<size_t, 7>(j)), (fib_correct<size_t, 7>(j)));
     }
+}
+
+TEST(correctness, bigtest1) {
+  size_t j = 10000;
+  EXPECT_EQ((fib<cpp_int, 10>(j)), (fib_correct<cpp_int, 10>(j)));
+  //std::cout << fib<cpp_int, 10>(j) << "\n";
+}
+
+TEST(correctness, bigtest2) {
+  size_t j = 10000;
+  EXPECT_EQ((fib<cpp_int, 10>(j)), (fib_correct<cpp_int, 10>(j)));
+  //std::cout << fib<cpp_int, 10>(j) << "\n";
+}
+
+TEST(correctness, bigtest4) {
+  size_t j = 100000;
+  EXPECT_EQ((fib<cpp_int, 2>(j)), (fib_correct<cpp_int, 2>(j)));
+  //std::cout << fib<cpp_int, 2>(j) << "\n";
+}
+
+TEST(correctness, bigtest5) {
+  //size_t j = 1000000;
+  //std::cout << fib<cpp_int, 2>(j) << "\n";
+}
+
+TEST(correctness, bigtest6) {
+  size_t j = 1000;
+  EXPECT_EQ((fib<cpp_int, 100>(j)), (fib_correct<cpp_int, 100>(j)));
+  //std::cout << fib<cpp_int, 100>(j) << "\n";
+}
+
+TEST(correctness, bigtest_matrix) {
+  for (size_t j = 0; j < 1000; ++j) {
+    //std::cout << j << "\n";
+    EXPECT_EQ((fib<cpp_int, 30>(j)), (fib_correct<cpp_int, 30>(j)));
+  }
+}
+
+TEST(correctness, bigtest_iterative) {
+  for (size_t j = 0; j < 1000; ++j) {
+    //std::cout << j << "\n";
+    EXPECT_EQ((fib_iterative<cpp_int, 30>(j)), (fib_correct<cpp_int, 30>(j)));
+  }
+}
+
+TEST(correctness, bigtest_matrix_2) {
+  for (size_t j = 0; j < 1000000; j += 200000) {
+    // std::cout << j << "\n";
+    fib<cpp_int, 2>(j);
+  }
+}
+
+//TEST(correctness, bigtest_iterative_2) {
+//  for (size_t j = 0; j < 1000000; j += 200000) {
+//    // std::cout << j << "\n";
+//    fib_iterative<cpp_int, 2>(j);
+//  }
+//}
+
+TEST(correctness, very_big) {
+  size_t j = 1000000000;
+  fib<cpp_int, 2>(j);
 }
